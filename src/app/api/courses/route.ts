0d5dddbe4cf97646bbb279837996a60cc2c7e680
sweetypie <141814482+Sweetypie28@users.noreply.github.com>
2024-09-26 13:23:57 +0700

    Update route.ts

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -5,11 +5,15 @@ import sleep from "sleep-promise";
 
 //GET http://localhost:3000/api/courses
 export const GET = async () => {
+  // รอ 1 วินาทีเพื่อจำลองการดีเลย์ (ใช้ sleep-promise)
   await sleep(1000);
+  
+  // เรียกใช้ฟังก์ชันอ่านฐานข้อมูล
   readDB();
+  
   return NextResponse.json({ 
     ok: true, 
-    // Type casting to "Database"
+    // แปลงชนิดข้อมูลของ "DB" เป็นชนิด "Database" และส่งค่ากลับไปยัง API response
     courses: (<Database>DB).courses 
   });
 };
